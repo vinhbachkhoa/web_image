c92b2221654d5f6c83f8107e08f1455f7b501bff
Le Quang Vinh <lequangvinh2891996@gmail.com>
2018-03-24 22:05:38 +0700

    connect database and get data25

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -9,11 +9,11 @@ export class ContentComponent implements OnInit {
 
   constructor(private contentSerivce: ContentService) { }
 
-  contents: any[] = [{title:""}];
+  contents: any[] = [];
   content: string="quang vinh";
   ngOnInit() {
     this.contentSerivce.getData().subscribe((res) => {
-      // this.content = res;
+      console.log(res);
       this.contents=res;
       console.log(this.contents);
     })
