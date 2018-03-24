ea4ae2914df49a55b8f27c7d56463b59c296af6c
Le Quang Vinh <lequangvinh2891996@gmail.com>
2018-03-24 10:22:35 +0700

    connect database and get data9

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const express = require("express");
 const app = express();
 
 /* cau hinh corss */
-const cors = require('cors')
+// const cors = require('cors')
 
 
-var corsOptions = {
-    origin: 'http://localhost:4200',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
-}
+// var corsOptions = {
+//     origin: 'http://localhost:4200',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+// }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.use((request, response, next) => {
     response.setHeader('Content-Type', 'application/json');
--- src/app/content/content.service.ts
+++ src/app/content/content.service.ts
@@ -6,9 +6,9 @@ import 'rxjs/add/operator/map';
 export class ContentService {
 
     constructor(private http: Http) { }
-    url = 'https://quangvinh.herokuapp.com/title'; // chu y la phai co http://
-    getData(): Observable<any> {
-        debugger;
-        return this.http.get(this.url).map((res: Response) => res.text());
-    }
+    // url = 'https://quangvinh.herokuapp.com/title'; // chu y la phai co http://
+    // getData(): Observable<any> {
+    //     debugger;
+    //     return this.http.get(this.url).map((res: Response) => res.text());
+    // }
 }
